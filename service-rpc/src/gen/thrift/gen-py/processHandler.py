22c95fe3aa6a3fc8a3b25f9c6eceec77e4a29cc3
cujung <changun.jung@gmail.com>
2023-12-12 00:29:42 +0900

    null for not str

--- service-rpc/src/gen/thrift/gen-py/processHandler.py
+++ service-rpc/src/gen/thrift/gen-py/processHandler.py
@@ -264,7 +264,7 @@ class dataProcessSparkHandler():
         #         rtnCols.append(colVal)
         for (colType, colVal) in zip(self.colTColumnType, nextRow):
             # nullVal = b'[NULL]' if colVal == None else b''
-            nullVal = b'\x00' if colVal == None else b''
+            nullVal = b'\x00' if colVal == None and colType == 'string' else b''
             if colVal == None:
                 colVal = [''] # values is []
             elif colType == 'string':
